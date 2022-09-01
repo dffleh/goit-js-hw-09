c14ed4f0082a6f9c35cb6d4c7168949dbc486b48
Alexander Shabanov <dffleh1995@gmail.com>
2022-09-01 19:08:32 +0300

    add

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,21 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+const firstDelay = document.querySelector('input[name=delay]');
+const delayStep = document.querySelector('input[name=step]');
+const amount = document.querySelector('input[name=amount]');
+
+form.addEventListener('submit', promiseFunction);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const shouldResolve = Math.random() > 0.3;
+            if (shouldResolve) {
+                resolve({});
+            } else {
+                reject({})
+            }
+        }, 2222);
+    })
 }
