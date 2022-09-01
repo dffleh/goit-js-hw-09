5191520672007253f222b3186bd260c1c58b421b
Alexander Shabanov <dffleh1995@gmail.com>
2022-09-01 16:34:32 +0300

    add timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -11,46 +11,72 @@ const refs = {
     seconds: document.querySelector('span[data-seconds]'),
 };
 
-let timerId = null;
-
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
+        if (selectedDates[0] < options.defaultDate) {
+            refs.btnStart.setAttribute('disabled', '')
+            Notiflix.Notify.failure("Please choose a date in the future")
+        } else {
+            refs.btnStart.removeAttribute('disabled', '')
+        }
         console.log(selectedDates[0]);
     },
 };
 
-flatpickr(refs.areaForInputDate, options);
+const fp = flatpickr(refs.areaForInputDate, options);
+
+const timer = {
+    timerId: null,
+    isActive: false,
+    start() {
+        if (this.isActive) {
+            return
+        }
+        const selectedDate = fp.selectedDates[0]
+        this.isActive = true
+        this.timerId = setInterval(() => {
+            const currentTime = Date.now()
+            const deltaTime = selectedDate - currentTime
+            const time = convertMs(deltaTime)
+            console.log(time);
+            updateClockFace(time)
+            if (deltaTime < 1000) {
+                clearInterval(this.timerId);
+                this.isActive = false;
+                Notiflix.Notify.info("Time is over")
+            }
+        }, 1000)
+    }
+};
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, "0")
+
+}
 
 function convertMs(ms) {
-    // Number of milliseconds per unit of time
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
-
-    // Remaining days
     const days = Math.floor(ms / day);
-    // Remaining hours
     const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
     const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
     return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+refs.btnStart.addEventListener('click', () => {
+    timer.start()
+});
 
 function updateClockFace({ days, hours, minutes, seconds }) {
     refs.days.textContent = `${days}`
     refs.hours.textContent = `${hours}`
     refs.minutes.textContent = `${minutes}`
     refs.seconds.textContent = `${seconds}`
-}
+};
